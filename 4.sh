2d4ec9f33cada1a7fbfb429c9d755bd72a6c83a6
dback31 <luis.konradt@outlook.de>
2017-07-07 22:09:50 +0200

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,2 +1,2 @@
 mkdir -p ~/octos
-repo init -u https://github.com/Team-OctOS/platform_manifest.git -b oct-14.1 && repo sync -j1
+repo init -u https://github.com/Team-OctOS/platform_manifest.git -b oct-14.1 && repo sync -j4
