8eee99335b61e1992762f8f976f591b6b959912c
dback31 <luis.konradt@outlook.de>
2017-07-07 22:04:20 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 echo Installing Dependencies!
 sudo apt update -y
-sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven
+sudo apt-get install git libcap-dev textinfo ccache automake autoconf libgmp-dev libexpat-dev lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx python-dev libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven
 echo Dependencies have been installed
